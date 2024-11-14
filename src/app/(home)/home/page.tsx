ab41ea1922c1caf82885dfcee6c02044f497a358
kinDreno <sepriothofficial@gmail.com>
2024-11-13 16:08:43 -0800

    will fix the typecasting later zzz

--- src/app/(home)/home/page.tsx
+++ src/app/(home)/home/page.tsx
@@ -3,41 +3,69 @@ import { Button } from "@/components/ui/button";
 import { IoMdStarOutline } from "react-icons/io";
 import { MdOutlineStar } from "react-icons/md";
 import { FaComment } from "react-icons/fa6";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { MainContent } from "@/types/here";
 
 const Page = () => {
   const [star, setStar] = useState<boolean>(false);
   const [char, setChar] = useState<number>(0);
+  const [datas, setDatas] = useState<MainContent[]>([]);
 
   const toggleStar = () => {
     setStar((prevStar) => !prevStar);
     setChar((prevChar) => (star ? prevChar - 1 : prevChar + 1));
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/posts/", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        const data = await response.json();
+        setDatas(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <main className="overflow-y-scroll h-full w-[80%] space-y-3 flex justify-center items-center">
-        <article className="h-full w-[70%] border-b-2 p-4 space-y-4">
-          <div>John Doe &#8226; 4 Days Ago &#8226; BSCS 1-1</div>
-          <section className="h-full w-full text-left space-y-3">
-            <h4 className="font-bold">Title Example Here</h4>
-            <h6>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Doloremque culpa labore, ullam itaque dolore temporibus eius
-              obcaecati sit sapiente praesentium iste dolorem est tenetur
-              debitis laudantium maiores illo, ratione voluptate?
-            </h6>
-            <section className="space-x-4 flex items-center">
-              <Button onClick={toggleStar}>
-                {!star ? <IoMdStarOutline /> : <MdOutlineStar />} {char}{" "}
-                Charisma
-              </Button>
-              <Button>
-                <FaComment /> Comment
-              </Button>
+        {datas.map((post, index) => (
+          <article
+            key={index}
+            className="h-full w-[70%] border-b-2 p-4 space-y-4"
+          >
+            <div>
+              {post.author} &#8226; {post.date} &#8226; {post.collegeYear}
+            </div>
+            <section className="h-full w-full text-left space-y-3">
+              <h4 className="font-bold">{post.title}</h4>
+              <h6>{post.content}</h6>
+              <section className="space-x-4 flex items-center">
+                <Button onClick={toggleStar}>
+                  {!star ? <IoMdStarOutline /> : <MdOutlineStar />} {char}{" "}
+                  Charisma
+                </Button>
+                <Button>
+                  <FaComment /> Comment
+                </Button>
+              </section>
             </section>
-          </section>
-        </article>
+          </article>
+        ))}
       </main>
     </>
   );
--- src/types/here.ts
+++ src/types/here.ts
@@ -26,9 +26,35 @@ export type SignIn = {
   course: string;
   collegeYear: string;
 };
-
 export type Post = {
   title: string;
   content: string;
   tag: string;
+  postedAt?: Date;
+  userId?: string;
+  comments?: Comment[];
+};
+
+export interface MainContent extends Post {
+  charisma?: number;
+}
+
+export type Comment = {
+  id: number;
+  text: string;
+  userId: string;
+  postId: number;
+  createdAt: Date;
+};
+
+export type User = {
+  id: number;
+  uid: string;
+  firstName: string;
+  lastName: string;
+  course: string;
+  collegeYear: string;
+  email: string;
+  posts: Post[];
+  comments: Comment[];
 };
