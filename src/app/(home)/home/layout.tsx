eac4f473034f650b5428f0dd73a1dc52462bab27
kinDreno <sepriothofficial@gmail.com>
2024-11-20 01:20:09 +0800

    the docs of supbse is updated and bugs occurs in this project

--- src/app/(home)/home/layout.tsx
+++ src/app/(home)/home/layout.tsx
@@ -1,36 +1,13 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { createClient } from "@/utils/supabase/client";
 import NavHome from "@/components/nav.home";
-import MobileHomeNav from "@/components/mobile-home-nav";
 import SidebarHome from "@/components/sidebar-home";
-import { SkeletonDemo } from "./skeleton";
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+import MobileHomeNav from "@/components/mobile-home-nav";
+import { createClient } from "@/utils/supabase/client";
+import { redirect } from "next/navigation";
+const Layout = async ({ children }: { children: React.ReactNode }) => {
   const supabase = createClient();
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      const { data, error } = await supabase.auth.getUser();
-      if (error || !data.user) {
-        router.push("/");
-      } else {
-        setLoading(false);
-      }
-    };
-
-    fetchUser();
-  }, [supabase, router]);
-
-  if (loading) {
-    return (
-      <div className="flex justify-center items-center h-screen w-full">
-        <SkeletonDemo />
-      </div>
-    );
+  const { data: data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    return redirect("/");
   }
 
   return (
@@ -45,4 +22,4 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-export default AuthLayout;
+export default Layout;
